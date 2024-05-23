8acc74d01b62fcabbafdc95af7aad98a8af6d191
Maria Monteiro <104790525+maria-monteiros@users.noreply.github.com>
2024-05-23 20:32:01 -0300

    commit initial

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const btnShare = document.querySelector('.botao')
+const openShare = document.querySelector('.share')
+
+btnShare.addEventListener('click', function () {
+    openShare.classList.toggle('open');
+})
+
+
+
+
